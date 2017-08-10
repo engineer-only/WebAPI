92e90e2eb70ac3a460eb0565e34413847f2b5d55
Irfan Can Kaleli <irfankaleli@yandex.com>
2017-08-10 16:46:33 +0300

    Google Sign-in Added

--- APIgit/public/js/main.js
+++ APIgit/public/js/main.js
@@ -27,5 +27,24 @@ $(document).ready(function()
         $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
         $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
     })
-
 });
+
+function onSignIn(googleUser) {
+    var glp = '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-user" aria-hidden="true">';
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    $('#userSection').html(profile.getName() + glp);
+}
+
+function signOut() {
+    var glp = '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-user" aria-hidden="true">';
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    $('#userSection').html("User"+ glp);
+}
